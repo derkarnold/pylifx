2a59eaf74da7a67abb062c5a0fb69ad65f4e6bed
Michael Farrell <micolous+git@gmail.com>
2014-03-30 20:55:32 +1030

    Use colorsys stdlib module instead of homebrew implementation.
    Conflicts:
    	pylifx/__init__.py

--- pylifx/__init__.py
+++ pylifx/__init__.py
@@ -15,7 +15,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-__all__ = ['colour', 'networking', 'packet', 'pylifx']
+__all__ = ['networking', 'packet', 'pylifx']
 __licence__ = '''
 Copyright (c) 2014, Deryck Arnold
 All rights reserved.
@@ -30,7 +30,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 '''
 
 
-from colour import rgbToHsb
+from colorsys import rgb_to_hsv
 from threading import Lock
 from thread import start_new_thread
 from networking import LifxBulbTCPServer, LifxUDPSocket, get_interface
@@ -97,7 +97,7 @@ class LifxController:
         
     def set_rgb(self, red, green, blue, fadeTime = 1):
         print 'Setting colour of %s to (R:%f, G:%f, B:%f) over %d seconds' % (self._name, red, green, blue, fadeTime)
-        hue, saturation, brightness = rgbToHsb(red, green, blue)
+        hue, saturation, brightness = rgb_to_hsv(red, green, blue)
         self._set_colour(hue, saturation, brightness, 0, fadeTime)
     
     def set_hsb(self, hue, saturation, brightness, fadeTime = 1):
